8e83d9f19944dcac9c05b5e908b83d8f6f07b8f6
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2016-02-29 14:50:01 +0100

    Initial.

---
+++ examples/commandline-client.js
@@ -0,0 +1,24 @@
+var dc = require('./../')({
+    roomName: 'chatRoom',
+    signallingServer: 'http://localhost:3000',
+    rtcOpts: {iceServers: [{urls: 'stun:stun.l.google.com:19302'}]},
+    debugMode: false
+});
+var rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+dc.on('ready', function () {
+    dc.on('channel:ready', function () {
+        console.log('Yeah we got a new data channel with a new peer.');
+    });
+
+    dc.on('message', function (data) {
+        console.log(data.sender + ': ' + data.text);
+    });
+
+    rl.on('line', function (cmd) {
+        dc.sendMessage(cmd);
+    });
+});
