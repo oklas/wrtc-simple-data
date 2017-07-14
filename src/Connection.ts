caaf0ccb9d584359c4e59eb40cc2c9c28287f777
Serguei Okladnikov <oklaspec@gmail.com>
2017-07-14 11:01:00 +0300

    linting

--- src/Connection.ts
+++ src/Connection.ts
@@ -32,13 +32,14 @@ export default class Connection {
         debugMode: opts.debugMode || false
     }
 
-    this.debug = this.options.debugMode ?
+    this.debug = ( this.options.debugMode ?
       function (msg: string) {
         return console.log('[DEBUG] ' + msg)
       } :
       function (msg ?: string) {
         return undefined
       }
+    )
 
     this.socket = SocketIoClient(this.options.signallingServer)
     this.myId = undefined
@@ -55,67 +56,70 @@ export default class Connection {
     const socket = this.socket
 
     this.socket.on('connect', () => {
-      this.socket.emit('join', this.room);
-      this.debug('Sent request to join room ' + this.room);
+      this.socket.emit('join', this.room)
+      this.debug('Sent request to join room ' + this.room)
 
       this.socket.on('created', (id) => {
-        this.debug('Created new room ' + this.room);
-      });
+        this.debug('Created new room ' + this.room)
+      })
 
         this.socket.on('joined', (data) => {
-          this.myId = data.id;
-          for (var i = -1; i < data.peers.length; i++) {
-            if (data.peers[i] != this.myId) {
-              this.createConnection(data.peers[i], false);
+          this.myId = data.id
+          for (let i = -1; i < data.peers.length; i++) {
+            if (data.peers[i] !== this.myId) {
+              this.createConnection(data.peers[i], false)
             }
           }
-          this.debug('Successfully joined room ' + this.room);
-          if (this._callbacks['ready']) {
-            this._callbacks['ready']();
+          this.debug('Successfully joined room ' + this.room)
+          const ready = 'ready'
+          if (this._callbacks[ready]) {
+            this._callbacks[ready]()
           }
-        });
+        })
 
         this.socket.on('new peer', (id) => {
-          this.debug('New peer has joined the room');
-          const pc = this.createConnection(id, true);
-          this.callPeer(pc);
-        });
+          this.debug('New peer has joined the room')
+          const pc = this.createConnection(id, true)
+          this.callPeer(pc)
+        })
 
         this.socket.on('data', (data) => {
           switch (data.type) {
             case 'offer':
-              this.debug('Got an offer from ' + data.from);
-              this.onOffer(data, this.pcMap[data.from]);
-              break;
+              this.debug('Got an offer from ' + data.from)
+              this.onOffer(data, this.pcMap[data.from])
+              break
             case 'answer':
-              this.debug('Got an answer from ' + data.from);
-              this.onAnswer(data, this.pcMap[data.from]);
-              break;
+              this.debug('Got an answer from ' + data.from)
+              this.onAnswer(data, this.pcMap[data.from])
+              break
             case 'candidate':
-              this.debug('Got a candidate: ' + data.id);
-              this.onCandidate(data.candidate, this.pcMap[data.id]);
-              break;
+              this.debug('Got a candidate: ' + data.id)
+              this.onCandidate(data.candidate, this.pcMap[data.id])
+              break
+            default:
+              this.debug(`Got an unexpected data type:'${data.type}'`)
           }
-        });
-    });
+        })
+    })
   }
 
   createConnection = (id, create) => {
-    const pc = new wrtc.RTCPeerConnection(this.options.rtcOps);
-    pc.id = id;
-    this.debug('Created peer connection ' + id);
+    const pc = new wrtc.RTCPeerConnection(this.options.rtcOps)
+    pc.id = id
+    this.debug('Created peer connection ' + id)
 
-    if (create) this.createDataChannel(pc);
+    if (create) this.createDataChannel(pc)
     else pc.ondatachannel = (event) => {
-      this.onDataChannel(event, pc);
-    };
+      this.onDataChannel(event, pc)
+    }
 
     pc.onicecandidate = (event) => {
-      this.handleIceCandidate(event, pc);
-    };
+      this.handleIceCandidate(event, pc)
+    }
 
-    this.pcMap[id] = pc;
-    return pc;
+    this.pcMap[id] = pc
+    return pc
   }
 
   createDataChannel = (peerconnection) => {
@@ -123,121 +127,122 @@ export default class Connection {
       this.options.channelName,
       this.options.channelOpts
     )
-    dataChannel.remotePeer = peerconnection;
-    this.dataChannels.push(dataChannel);
-    this.setDataChannelCallbacks(dataChannel);
-    this.debug('Created data channel with peer ' + peerconnection.id);
+    dataChannel.remotePeer = peerconnection
+    this.dataChannels.push(dataChannel)
+    this.setDataChannelCallbacks(dataChannel)
+    this.debug('Created data channel with peer ' + peerconnection.id)
   }
 
   callPeer = (pc) => {
     pc.createOffer((description) => {
-      description.from = this.myId;
-      description.to = pc.id;
-      this.setLocalDescription(description, pc);
-    }, console.log);
-    this.debug('Created offer for peer ' + pc.id);
+      description.from = this.myId
+      description.to = pc.id
+      this.setLocalDescription(description, pc)
+    }, console.log)
+    this.debug('Created offer for peer ' + pc.id)
   }
 
   onOffer = (description, pc) => {
-    pc.setRemoteDescription(new wrtc.RTCSessionDescription(description));
-    this.debug('Set remote description for peer ' + pc.id);
+    pc.setRemoteDescription(new wrtc.RTCSessionDescription(description))
+    this.debug('Set remote description for peer ' + pc.id
+    /* tslint:disable:no-shadowed-variable */
     pc.createAnswer((description) => {
-      description.from = this.myId;
-      description.to = pc.id;
-      this.setLocalDescription(description, pc);
-    }, console.log);
+      description.from = this.myId
+      description.to = pc.id
+      this.setLocalDescription(description, pc)
+    }, console.log)
   }
 
   onAnswer = (description, pc) => {
-    pc.setRemoteDescription(new wrtc.RTCSessionDescription(description));
-    this.debug('Set remote description for peer ' + description.from);
+    pc.setRemoteDescription(new wrtc.RTCSessionDescription(description))
+    this.debug('Set remote description for peer ' + description.from)
   }
 
   setLocalDescription = (description, pc) => {
-    pc.setLocalDescription(description);
-    this.debug('Set local description for ' + pc.id + ' and sent offer / answer.');
-    this.socket.emit('data', description);
+    pc.setLocalDescription(description)
+    this.debug('Set local description for ' + pc.id + ' and sent offer / answer.')
+    this.socket.emit('data', description)
   }
 
   handleIceCandidate = (event, pc) => {
-    if (pc.candidateSent || !event.candidate) return;
-    var candidate = event.candidate;
+    if (pc.candidateSent || !event.candidate) return
+    const candidate = event.candidate
     this.socket.emit('data', {
       type: 'candidate',
       candidate: candidate,
       id: this.myId,
-    });
-    this.debug('Broadcasted candidate: ' + pc.id);
-    pc.candidateSent = true;
+    })
+    this.debug('Broadcasted candidate: ' + pc.id)
+    pc.candidateSent = true
   }
 
   onCandidate = (candidate, pc) => {
-    if (!candidate) return;
+    if (!candidate) return
     pc.addIceCandidate(new wrtc.RTCIceCandidate({
       sdpMLineIndex: candidate.sdpMLineIndex,
       sdpMid: candidate.sdpMid,
       candidate: candidate.candidate,
-    }));
-    this.debug('Added received candidate ' + pc.id);
+    }))
+    this.debug('Added received candidate ' + pc.id)
   }
 
   setDataChannelCallbacks = (dataChannel) => {
     dataChannel.onopen = () => {
-      this.handleDataChannelState(dataChannel);
-    };
+      this.handleDataChannelState(dataChannel)
+    }
     dataChannel.onclose = () => {
-      this.handleDataChannelState(dataChannel);
-    };
+      this.handleDataChannelState(dataChannel)
+    }
     dataChannel.onmessage = (event) => {
-      this.onMessage(event, dataChannel);
-    };
-    this.debug('Set the data channel callback.');
+      this.onMessage(event, dataChannel)
+    }
+    this.debug('Set the data channel callback.')
   }
 
   handleDataChannelState = (dataChannel) => {
-      var state = dataChannel.readyState;
-      this.debug('Channel is ' + state);
-      if (this._callbacks['channel:ready'] && state == 'open') {
-        this._callbacks['channel:ready']();
-      }
-      else if (this._callbacks['channel:notready'] && state != 'open') {
-        this._callbacks['channel:notready']();
+      const state = dataChannel.readyState
+      this.debug('Channel is ' + state)
+      if (this._callbacks['channel:ready'] && state === 'open') {
+        this._callbacks['channel:ready']()
+      } else if (this._callbacks['channel:notready'] && state !== 'open') {
+        this._callbacks['channel:notready']()
       }
   }
 
   onDataChannel = (event, pc) => {
-      const dataChannel = event.channel;
-      dataChannel.remotePeer = pc;
-      this.dataChannels.push(dataChannel);
+      const dataChannel = event.channel
+      dataChannel.remotePeer = pc
+      this.dataChannels.push(dataChannel)
       this.setDataChannelCallbacks(dataChannel)
   }
 
   onMessage = (event, dc) => {
-      this.debug('[Message] ' + event.data);
-      if (this._callbacks['message']) {
-        this._callbacks['message']({
+      this.debug('[Message] ' + event.data)
+      const message = 'message'
+      if (this._callbacks[message]) {
+        this._callbacks[message]({
           text: event.data,
           sender: dc.remotePeer.id
-        });
+        })
       }
   }
 
   on = (event, callback) => {
-      this._callbacks[event] = callback;
+      this._callbacks[event] = callback
   }
 
   sendMessage = (message) => {
       this.dataChannels.forEach(function (channel) {
-          channel.send(message);
-      });
-      this.debug('Sent message');
+          channel.send(message)
+      })
+      this.debug('Sent message')
   }
 
   close = () => {
     Object.keys(this.pcMap).forEach((id) => {
       this.pcMap[id].close()
     })
-    this.socket.close();
+    this.socket.close()
   }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export default from './Connection'
+// workaround https://github.com/Microsoft/TypeScript/issues/17186
+// export default from './Connection'
+// export * from './Connection'
+import Connection from './Connection'
+export default Connection
